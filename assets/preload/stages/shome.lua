e573b7bdc6e02bd6bc4744300e9ee6684b5e13d3
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2022-05-15 01:29:24 +0300

    Add files via upload

---
+++ assets/preload/stages/shome.lua
@@ -0,0 +1,12 @@
+function onCreate()
+	
+      makeLuaSprite('shome', 'shome', -800, -290);
+	addLuaSprite('shome', false)
+	setScrollFactor('shome', 1, 1);
+	scaleObject('shome', 0.85,0.85)
+	
+-- Your Custom bg sprites or something like this
+	
+	 close(true);
+
+end
